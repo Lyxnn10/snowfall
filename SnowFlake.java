fffcc76ce31b39e5aad136d5ee9a857b640714af
Lyxnn10 <93597031+Lyxnn10@users.noreply.github.com>
2023-01-25 10:14:16 +0100

    Update SnowFlake.java

--- SnowFlake.java
+++ SnowFlake.java
@@ -3,11 +3,12 @@ class SnowFlake{
   float x = random(width);
   float y = random(height);
   float r = random(10);
-
+  float v = random(1,10);
+  
   void draw(){
    ellipse(x, y, r, r);
 
-   y++;
+   y+=v;
 
    if(y > height){
      y = 0;
